1e5a44c3fa30aee8424819064d217fa873fa2dcf
0xAC <0xacioi@gmail.com>
2024-06-10 14:22:17 +0100

    Initial Commit

---
+++ BrainStorm/BrainStormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainStormApp.swift
+//  BrainStorm
+//
+//  Created by Arnold on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrainStormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
